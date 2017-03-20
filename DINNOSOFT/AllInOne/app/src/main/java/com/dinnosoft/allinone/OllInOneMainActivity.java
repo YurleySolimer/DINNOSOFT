5283369a50d4bd1b221fad6e5c949b73bb55fcaa
Carlos Ramirez <carlosduardramirez@gmail.com>
2017-03-20 02:20:28 -0400

    Version 1.0 Diagramas de casos de uso

---
+++ DINNOSOFT/AllInOne/app/src/main/java/com/dinnosoft/allinone/OllInOneMainActivity.java
@@ -0,0 +1,13 @@
+package com.dinnosoft.allinone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OllInOneMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oll_in_one_main);
+    }
+}
